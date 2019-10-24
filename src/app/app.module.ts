0e82c3ebbdc16c9ef226b873d5b5d56347f5c53d
Dave Calderwood <davidhcalderwood@gmail.com>
2019-10-24 15:26:06 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { LeftNavComponent } from './left-nav/left-nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LeftNavComponent
   ],
   imports: [
     BrowserModule,
